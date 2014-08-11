3c18526e61f4f45514034193c8c57287b32d0a8a
oozzal <theoozzal@gmail.com>
2014-08-11 16:41:51 +0545

    Routes and websocket config.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  post 'home/set_username'
+
+  get 'home/logout'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
